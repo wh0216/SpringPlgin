742f5762ac5cf4465129df61f44862688060f5fc
wh0216 <1520577322@qq.com>
2023-01-20 20:59:02 +0800

    elastucsearch中间件

--- es-SpringBoot/src/main/java/com/wh/es/EsHandTest.java
+++ es-SpringBoot/src/main/java/com/wh/es/EsHandTest.java
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
